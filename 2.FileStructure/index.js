5df5b710a176422e8bba0d7ffe5cc77661be2c83
userFbi <pawartushar2024.katargam@gmail.com>
2026-03-10 08:32:15 +0530

    Initial Commit

---
+++ 2.FileStructure/index.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+
+//fs.writeFileSync('demo.txt' , "Hellooooo")
+
+console.log("hello");
+console.log("hello23");
+
+// const data = fs.readFileSync('demo.txt','utf-8')
+// console.log(data);
+
+// fs.appendFileSync('demo.txt',' This is append mode')
+
+fs.unlinkSync("demo.txt");
+
+console.log("hello235");
+console.log("hello23578");
---
+++ 3.Express/app.js
@@ -0,0 +1,8 @@
+const express = require("express");
+
+const app = express;
+
+app.get("/", (res, req) => {
+  res.send("This is HomePage");
+});
+app.listen(3000);
