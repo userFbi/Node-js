dd71c87c107c04d348c2bd37f33b86dc96c41c4d
userFbi <pawartushar2024.katargam@gmail.com>
2026-03-12 09:42:57 +0530

    learned edit and delete part

--- 4.Crud/app.js
+++ 4.Crud/app.js
@@ -3,7 +3,7 @@ const app = express();
 const fs = require("fs");
 
 let stData = [];
-
+let editid = null;
 let fileData = fs.readFileSync("data.json", "utf-8");
 
 if (fileData != "") {
@@ -11,16 +11,40 @@ if (fileData != "") {
 }
 
 app.get("/", (req, res) => {
-  res.render("index.ejs", { stData });
+  res.render("index.ejs", { stData, edit: null });
 });
+
 app.get("/createData", (req, res) => {
+  // get
   const data = req.query;
   console.log(data);
 
-  stData.push(data);
+  if (editid != null) {
+    stData[editid] = data;
+    editid = null;
+  } else {
+    stData.push(data);
+  }
+
   fs.writeFileSync("data.json", JSON.stringify(stData));
 
   res.redirect("/");
+});
+
+app.get("/deleteData/:deleteId", (req, res) => {
+  const id = req.params.deleteId;
+
+  stData.splice(id, 1);
+  fs.writeFileSync("data.json", JSON.stringify(stData));
+  res.redirect("/");
+});
+
+app.get("/updateData/:editId", (req, res) => {
+  editid = req.params.editId;
+  const edit = stData[editid];
+  console.log(edit);
+
+  res.render("index.ejs", { edit, stData });
 });
 
 app.listen(3000);
