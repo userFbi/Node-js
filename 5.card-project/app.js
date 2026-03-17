fafde9095a33639521e7bedc7306f6d06c68055d
userFbi <pawartushar2024.katargam@gmail.com>
2026-03-17 09:26:44 +0530

    card-project completed

--- 5.card-project/app.js
+++ 5.card-project/app.js
@@ -1,8 +1,25 @@
 const express = require("express");
 const app = express();
+const fs = require("fs");
+
+let stData = [];
+let fileData = fs.readFileSync("data.json", "utf-8");
+if (fileData != "") {
+  stData = JSON.parse(fileData);
+}
 
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+  res.render("index.ejs", { stData });
+
+  fs.writeFileSync("data.json", JSON.stringify(stData));
+});
+
+app.get("/cardDetail/:cardId", (req, res) => {
+  const index = req.params.cardId;
+  console.log("index:", index);
+  const userData = stData.find((item) => item.id == index);
+  console.log(userData);
+  res.render("product.ejs", { userData });
 });
 
 app.listen(3000);
